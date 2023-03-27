9feafa3eda977d822a6270b3270e0f407763ff1c
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-03-28 00:50:34 +0900

    App 실행 확인

---
+++ Untitled/Controller/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  Untitled
+//
+//  Created by Jaehyeok Lim on 2023/03/28.
+//
+
+import UIKit
+
+class mainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewLayout()
+        
+    }
+    
+    private func viewLayout() {
+        view.backgroundColor = UIColor.black
+    }
+}
