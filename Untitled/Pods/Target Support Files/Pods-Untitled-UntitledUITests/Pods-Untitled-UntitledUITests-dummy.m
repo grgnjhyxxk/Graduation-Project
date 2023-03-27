17e7da507238997b55b102615b73fd2f3a807f31
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-03-28 00:52:49 +0900

    cocoa pod init, snap kit 설치

---
+++ Untitled/Pods/Target Support Files/Pods-Untitled-UntitledUITests/Pods-Untitled-UntitledUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Untitled_UntitledUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Untitled_UntitledUITests
+@end
---
+++ Untitled/Pods/Target Support Files/Pods-Untitled/Pods-Untitled-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Untitled : NSObject
+@end
+@implementation PodsDummy_Pods_Untitled
+@end
