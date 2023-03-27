f3bd1e08f7a30cb103bf572d3f1c2cd35127be5c
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-03-28 00:59:17 +0900

    MainViewController Name 변경 및 핵심 ViewController 3개(Init, Alarm, AiRecommended) 추가

---
+++ Untitled/Controller/AlarmViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AlarmViewController.swift
+//  Untitled
+//
+//  Created by Jaehyeok Lim on 2023/03/28.
+//
+
+import UIKit
+
+class AlramViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewLayout()
+        
+    }
+    
+    private func viewLayout() {
+        view.backgroundColor = UIColor.black
+    }
+}
---
+++ Untitled/Controller/InitViewController.swift
@@ -0,0 +1,8 @@
+//
+//  InitViewController.swift
+//  Untitled
+//
+//  Created by Jaehyeok Lim on 2023/03/28.
+//
+
+import Foundation
