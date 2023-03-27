1c8df140c0d5bc3f3a864583ad98f4874ce480dd
jaehyeoklim <jaehyeoklim.developher@gmail.com>
2023-03-28 03:10:50 +0900

    아이디/비밀번호 찾기, 회원가입 Label View에 추가

--- Untitled/Controller/InitViewController.swift
+++ Untitled/Controller/InitViewController.swift
@@ -17,6 +17,8 @@ class initViewController: UIViewController {
     let initViewRingShapeLayer = initView().initViewRingShapeLayer()
     let subTextLabelInInitViewRingShapeLayer = initView().subTextLabelInInitViewRingShapeLayer()
     let loginButton = initView().loginButton()
+    let findIdPasswordTextLabel = initView().findIdPasswordTextLabel()
+    let signUpTextLabel = initView().signUpTextLabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,10 +54,20 @@ class initViewController: UIViewController {
             make.trailing.equalTo(view).offset(-91)
             make.size.height.equalTo(33)
         }
+        
+        findIdPasswordTextLabel.snp.makeConstraints { make in
+            make.top.equalTo(loginButton.snp.bottom).offset(10)
+            make.leading.equalTo(loginButton.snp.leading).offset(0.5)
+        }
+        
+        signUpTextLabel.snp.makeConstraints { make in
+            make.top.equalTo(loginButton.snp.bottom).offset(10)
+            make.trailing.equalTo(loginButton.snp.trailing).offset(-0.5)
+        }
     }
     
     private func addSubview() {
-        uiViewList = [mainTextLabel, subTextLabel, subTextLabelInInitViewRingShapeLayer, loginButton]
+        uiViewList = [mainTextLabel, subTextLabel, subTextLabelInInitViewRingShapeLayer, loginButton, findIdPasswordTextLabel, signUpTextLabel]
         
         for uiView in uiViewList {
             view.addSubview(uiView)
