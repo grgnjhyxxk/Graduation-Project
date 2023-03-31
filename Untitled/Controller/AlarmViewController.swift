8069509cc2a4e2aa6e6e9eb69fa940fc105927cc
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-04-01 04:48:50 +0900

    titleTextButton 추가 및 appIconForViewImageView 삭제

--- Untitled/Controller/AlarmViewController.swift
+++ Untitled/Controller/AlarmViewController.swift
@@ -12,8 +12,8 @@ class alarmViewController: UIViewController {
     var uiViewList: [UIView] = []
 
     let userProfileButton = commonView().roundingButton()
-    let appIconForViewImageView = commonView().appIconForViewImageView()
     let envelopeButton = commonView().envelopeButton()
+    let titleTextButton = commonView().titleTextButton(titleText: "알람관리")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,21 +31,21 @@ class alarmViewController: UIViewController {
             make.size.equalTo(CGSize(width: 45, height: 45))
         }
         
-        appIconForViewImageView.snp.makeConstraints { make in
-            make.top.equalTo(view).offset(74)
-            make.leading.equalTo(view).offset(31)
-            make.size.equalTo(CGSize(width: 45, height: 45))
-        }
-        
         envelopeButton.snp.makeConstraints { make in
             make.top.equalTo(userProfileButton.snp.top).offset(10)
             make.trailing.equalTo(userProfileButton.snp.leading).offset(-10)
             make.size.equalTo(CGSize(width: 36, height: 26))
         }
+        
+        titleTextButton.snp.makeConstraints { make in
+            make.top.equalTo(view).offset(76)
+            make.leading.equalTo(view).offset(31)
+            make.size.equalTo(CGSize(width: 122, height: 44))
+        }
     }
     
     private func addSubview() {
-        uiViewList = [userProfileButton, appIconForViewImageView, envelopeButton]
+        uiViewList = [userProfileButton, envelopeButton, titleTextButton]
         
         for uiView in uiViewList {
             view.addSubview(uiView)
