00e48702599b56f720ac9fb162f4d2d5eb2f4f04
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-03-31 17:58:09 +0900

    유연한 uiView 추가를 위한 배열 생성 및 관련 함수 작성, appIconForViewImageView 추가

--- Untitled/Controller/HomeViewController.swift
+++ Untitled/Controller/HomeViewController.swift
@@ -10,24 +10,39 @@ import SnapKit
 
 class homeViewController: UIViewController {
     
+    var uiViewList: [UIView] = []
+
     let userProfileButton = mainView().roundingButton()
+    let appIconForViewImageView = mainView().appIconForViewImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewLayout()
         setSameBackgroundColor()
-        
+        addSubview()
+        viewLayout()
     }
     
     private func viewLayout() {
         view.backgroundColor = UIColor.white
-        
-        view.addSubview(userProfileButton)
-        
+                
         userProfileButton.snp.makeConstraints { make in
             make.top.equalTo(view).offset(74)
             make.trailing.equalTo(view).offset(-31)
             make.size.equalTo(CGSize(width: 45, height: 45))
+        }
+        
+        appIconForViewImageView.snp.makeConstraints { make in
+            make.top.equalTo(view).offset(74)
+            make.leading.equalTo(view).offset(31)
+            make.size.equalTo(CGSize(width: 45, height: 45))
+        }
+    }
+    
+    private func addSubview() {
+        uiViewList = [userProfileButton, appIconForViewImageView]
+        
+        for uiView in uiViewList {
+            view.addSubview(uiView)
         }
     }
 }
