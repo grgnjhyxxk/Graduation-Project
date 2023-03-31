0f402802e5864b355634d9557a7ac16830222226
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-04-01 04:02:57 +0900

    공통 사항이 있는 코드 CommonView로 이동 및 삭제

--- Untitled/View/MainView.swift
+++ Untitled/View/MainView.swift
@@ -7,53 +7,6 @@
 
 import UIKit
 
-private let userProfileImageString = "TemporaryUserProfilePicture"
-private let appIconForViewImageString = "AppIconForOnView"
-
 class mainView: UIView {
-    
-    func roundingImage(string: String) -> UIImage {
-        let originalImage = UIImage(named: "\(string)")
 
-        // 이미지 크기와 동일한 새로운 이미지 생성
-        let imageFrame = CGRect(x: 0, y: 0, width: originalImage!.size.width, height: originalImage!.size.height)
-        UIGraphicsBeginImageContextWithOptions(imageFrame.size, false, 0.0)
-        let context = UIGraphicsGetCurrentContext()
-        context?.saveGState()
-        UIBezierPath(roundedRect: imageFrame, cornerRadius: imageFrame.size.width/2.0).addClip()
-        originalImage?.draw(in: imageFrame)
-        context?.restoreGState()
-        let roundedImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        return roundedImage
-    }
-    
-    func roundingButton() -> UIView {
-    
-        let image = roundingImage(string: userProfileImageString)
-        
-        let button = UIButton()
-        button.setImage(image, for: .normal)
-        button.contentVerticalAlignment = .fill
-        button.contentHorizontalAlignment = .fill
-        button.layer.cornerRadius = 22.5
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOpacity = 0.25
-        button.layer.shadowOffset = CGSize(width: 0.5, height: 3)
-        button.layer.shadowRadius = 5
-        button.layer.masksToBounds = false
-        button.layer.borderWidth = 1.8
-        button.layer.borderColor = UIColor(red: 133/255, green: 185/255, blue: 255/255, alpha: 0.45).cgColor
-        
-        return button
-    }
-    
-    func appIconForViewImageView() -> UIImageView {
-        let imageView = UIImageView()
-        
-        imageView.image = roundingImage(string: appIconForViewImageString)
-        
-        return imageView
-    }
 }
