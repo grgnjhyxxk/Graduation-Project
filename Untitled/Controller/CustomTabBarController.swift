15e08a8e406e6e9d845e319b174a65a48c186936
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-03-31 17:53:01 +0900

    CustomTabBarController 추가 및 스타일 추가

---
+++ Untitled/Controller/CustomTabBarController.swift
@@ -0,0 +1,44 @@
+//
+//  CustomTabBarController.swift
+//  Untitled
+//
+//  Created by Jaehyeok Lim on 2023/03/31.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    let addViewControllerNavigationButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureLayout()
+    }
+    
+    func configureLayout() {
+        self.tabBar.barTintColor = UIColor.white.withAlphaComponent(0.25)
+        self.tabBar.tintColor = UIColor.black.withAlphaComponent(0.6)
+        self.tabBar.unselectedItemTintColor = UIColor.gray.withAlphaComponent(0.5)
+        self.tabBar.shadowImage = UIImage()
+        self.tabBar.backgroundImage = UIImage()
+        
+        let firstViewController = homeViewController()
+        let secondViewController = alarmViewController()
+        let thirdViewController = aiRecommendedViewController()
+        
+        firstViewController.tabBarItem.selectedImage = UIImage(systemName: "house.fill")
+        firstViewController.tabBarItem.image = UIImage(systemName: "house")
+        
+        secondViewController.tabBarItem.selectedImage = UIImage(systemName: "shippingbox.fill")
+        secondViewController.tabBarItem.image = UIImage(systemName: "shippingbox")
+        
+        thirdViewController.tabBarItem.selectedImage = UIImage(systemName: "arrow.2.squarepath")
+        thirdViewController.tabBarItem.image = UIImage(systemName: "arrow.2.squarepath")
+        
+        viewControllers = [firstViewController, secondViewController, thirdViewController]
+    }
+}
+
+
