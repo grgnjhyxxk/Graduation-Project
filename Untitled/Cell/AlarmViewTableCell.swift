3e64a4f8d01bee19e771522d1130351a32fae693
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-01 05:50:52 +0900

    initial commit

---
+++ Untitled/Cell/AlarmViewTableCell.swift
@@ -0,0 +1,8 @@
+//
+//  AlarmViewTableCell.swift
+//  Untitled
+//
+//  Created by Jaehyeok Lim on 2023/05/01.
+//
+
+import Foundation
---
+++ Untitled/Model/AlarmCellDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlarmModel.swift
+//  Untitled
+//
+//  Created by Jaehyeok Lim on 2023/04/30.
+//
+
+import Foundation
