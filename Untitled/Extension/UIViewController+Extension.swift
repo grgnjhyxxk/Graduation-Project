17aeebd24282bcc9fba4de5188e6ebd8a71f9901
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-02 00:56:15 +0900

    setSameBackgroundColor 함수 수정

--- Untitled/Extension/UIViewController+Extension.swift
+++ Untitled/Extension/UIViewController+Extension.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 extension UIViewController {
-    func setSameBackgroundColor() {
+    func setSameBackgroundColor(firstAlpha: CGFloat, secondAlpha: CGFloat) {
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
-        gradientLayer.colors = [UIColor(red: 38/255, green: 151/255, blue: 255/255, alpha: 0.57).cgColor, UIColor(red: 171/255, green: 79/255, blue: 13/255, alpha: 0.57).cgColor]
+        gradientLayer.colors = [UIColor(red: 38/255, green: 151/255, blue: 255/255, alpha: firstAlpha).cgColor, UIColor(red: 171/255, green: 79/255, blue: 13/255, alpha: secondAlpha).cgColor]
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
         view.layer.insertSublayer(gradientLayer, at: 0)
