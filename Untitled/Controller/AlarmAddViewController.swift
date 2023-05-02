4c5fd14c958e52cffe485a991f63bdd03d1f23a0
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-02 18:56:34 +0900

    table View 추가 및 레이아웃 수정

--- Untitled/Controller/AlarmAddViewController.swift
+++ Untitled/Controller/AlarmAddViewController.swift
@@ -11,16 +11,21 @@ import SnapKit
 class alarmAddViewController: UIViewController {
     
     var uiViewList: [UIView] = []
+    var commonViewList: [UIView] = []
     
     let titleLabel = alarmAddView().titleLabel()
     let datePicker = alarmAddView().datePicker()
     let commonUiView = commonView().commonUiView(backgroundColor: UIColor.black.withAlphaComponent(0.07), borderWidth: 1, borderColor: UIColor(white: 1.0, alpha: 0.14), cornerRadius: 15)
+    let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setSameBackgroundColor(firstAlpha: 0.57, secondAlpha: 0.57)
         addSubview()
         viewLayout()
+        addOnCommonUiView()
+        commonUIViewLayout()
+        tableViewLayout()
     }
     
     private func viewLayout() {
@@ -33,25 +38,73 @@ class alarmAddViewController: UIViewController {
         
         datePicker.snp.makeConstraints { make in
             make.top.equalTo(titleLabel).offset(35)
-            make.leading.equalTo(0)
-            make.trailing.equalTo(0)
+            make.leading.equalTo(10)
+            make.trailing.equalTo(-10)
             make.height.equalTo(200)
         }
         
         commonUiView.snp.makeConstraints { make in
             make.top.equalTo(datePicker.snp.bottom).offset(10)
-            make.leading.equalTo(10)
-            make.trailing.equalTo(-10)
-            make.height.equalTo(200)
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.height.equalTo(160)
         }
     }
     
     private func addSubview() {
-        uiViewList = [titleLabel, datePicker, commonUiView]
+        uiViewList = [titleLabel, datePicker, commonUiView, tableView]
         
         for uiView in uiViewList {
             view.addSubview(uiView)
         }
     }
+    
+    private func commonUIViewLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalTo(commonUiView)
+        }
+    }
+    
+    private func addOnCommonUiView() {
+        commonViewList = [tableView]
+        
+        for uiView in commonViewList {
+            commonUiView.addSubview(uiView)
+        }
+    }
+    
+    private func tableViewLayout() {
+        tableView.register(alarmAddViewTableCell.self, forCellReuseIdentifier: "alarmAddViewTableCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = UIColor.clear
+        tableView.separatorStyle = .singleLine
+        tableView.showsVerticalScrollIndicator = false
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        tableView.isScrollEnabled = false
+        tableView.allowsSelection = false
+    }
 }
 
+extension alarmAddViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return AlarmAddViewCellData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "alarmAddViewTableCell", for: indexPath) as! alarmAddViewTableCell
+        
+        cell.clipsToBounds = true
+        cell.backgroundColor = UIColor.clear
+        
+        let data = AlarmAddViewCellData[indexPath.row]
+        cell.titleLabel.text = data.title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+}
