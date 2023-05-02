7af174fa70b1fd6d3e668ba1ffb9ba0b8f081dc3
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-02 18:54:26 +0900

    initial commit

---
+++ Untitled/Model/AlarmAddViewCellDataModel.swift
@@ -0,0 +1,19 @@
+//
+//  AlarmAddViewCellDataModel.swift
+//  Untitled
+//
+//  Created by Jaehyeok Lim on 2023/05/02.
+//
+
+import UIKit
+
+struct AlarmAddViewCellDataModel {
+    let title: String
+}
+
+let AlarmAddViewCellData = [
+    AlarmAddViewCellDataModel(title: "반복"),
+    AlarmAddViewCellDataModel(title: "레이블"),
+    AlarmAddViewCellDataModel(title: "사운드"),
+    AlarmAddViewCellDataModel(title: "다시 알림")
+]
