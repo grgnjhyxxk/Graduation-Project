0f382c19cf2053a5d9ff3bd28cdff50eca0c0976
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-03 03:56:58 +0900

    repeatSwitch 버튼 추가 및 레이아웃 조정, allHidenInit 함수 추가

--- Untitled/Cell/AlarmAddViewTableCell.swift
+++ Untitled/Cell/AlarmAddViewTableCell.swift
@@ -10,6 +10,8 @@ import SnapKit
 
 class alarmAddViewTableCell: UITableViewCell {
     
+    var contentViewList: [UIView] = []
+    
     let titleLabel: UILabel = {
         let label = UILabel()
         
@@ -24,7 +26,7 @@ class alarmAddViewTableCell: UITableViewCell {
         let textField = UITextField()
         
         textField.backgroundColor = UIColor.clear
-        textField.placeholder = "알림"
+        textField.placeholder = "알람"
         textField.textColor = UIColor.placeholderText
         textField.textAlignment = .right
         textField.font = UIFont.boldSystemFont(ofSize: 17)
@@ -32,25 +34,49 @@ class alarmAddViewTableCell: UITableViewCell {
         return textField
     }()
     
+    let repeatSwitch: UISwitch = {
+        let repeatSwitch = UISwitch()
+        
+        repeatSwitch.isOn = false
+        repeatSwitch.onTintColor = UIColor.applicationPointColorHighlightColor
+        return repeatSwitch
+    }()
     
     private func layout() {
-        contentView.addSubview(titleLabel)
-        contentView.addSubview(textField)
-        
         titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(10.5)
+            make.top.equalTo(12)
             make.leading.equalTo(12)
         }
         
         textField.snp.makeConstraints { make in
-            make.top.equalTo(9)
+            make.top.equalTo(10.5)
+            make.trailing.equalTo(-10)
+        }
+        
+        repeatSwitch.snp.makeConstraints { make in
+            make.top.equalTo(6.5)
             make.trailing.equalTo(-10)
         }
     }
     
+    private func addSubview() {
+        contentViewList = [titleLabel, textField, repeatSwitch]
+        
+        for uiView in contentViewList {
+            contentView.addSubview(uiView)
+        }
+    }
+    
+    private func allHidenInit() {
+        textField.isHidden = true
+        repeatSwitch.isHidden = true
+    }
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview()
         layout()
+        allHidenInit()
     }
     
     required init?(coder aDecoder: NSCoder) {
