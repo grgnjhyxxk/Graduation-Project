eb3c78f7cdaab5129374cd9d1611325abc4b093e
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-03 03:58:54 +0900

    test를 위한 수정

--- Untitled/Model/AlarmViewCellDataModel.swift
+++ Untitled/Model/AlarmViewCellDataModel.swift
@@ -5,4 +5,10 @@
 //  Created by Jaehyeok Lim on 2023/04/30.
 //
 
-import Foundation
+import UIKit
+
+struct AlarmViewCellDataModel {
+    let label: String
+    let repeatDays: String
+    let repeatSwitchState: Bool
+}
