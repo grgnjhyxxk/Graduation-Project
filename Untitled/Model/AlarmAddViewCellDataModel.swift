747df866b70a5ebfd5000240972b8846d8b09f64
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-03 21:12:18 +0900

    사운드 -> 사용자 변경

--- Untitled/Model/AlarmAddViewCellDataModel.swift
+++ Untitled/Model/AlarmAddViewCellDataModel.swift
@@ -14,6 +14,6 @@ struct AlarmAddViewCellDataModel {
 let AlarmAddViewCellData = [
     AlarmAddViewCellDataModel(title: "반복"),
     AlarmAddViewCellDataModel(title: "레이블"),
-    AlarmAddViewCellDataModel(title: "사운드"),
+    AlarmAddViewCellDataModel(title: "사용자"),
     AlarmAddViewCellDataModel(title: "다시 알림")
 ]
