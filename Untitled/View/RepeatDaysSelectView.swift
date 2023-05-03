e22aa5a73da1a8b3ce615d58f8c7b484583df179
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-04 02:13:23 +0900

    title Label, backButton 추가

--- Untitled/View/RepeatDaysSelectView.swift
+++ Untitled/View/RepeatDaysSelectView.swift
@@ -5,4 +5,28 @@
 //  Created by Jaehyeok Lim on 2023/05/04.
 //
 
-import Foundation
+import UIKit
+
+class repeatDaysSelectView: UIView {
+    
+    func titleLabel() -> UILabel {
+        let label = UILabel()
+            
+        label.text = "반복"
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textAlignment = .center
+        
+        return label
+    }
+    
+    func backButton() -> UIButton {
+        let button = UIButton()
+        
+        button.setTitle("뒤로", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        
+        return button
+    }
+}
+
