3d6f29b533b970f1fdf11a081e96ef643a88ebc8
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-04 03:23:29 +0900

    titleLabel과 close, saveButton 삭제

--- Untitled/View/AlarmAddView.swift
+++ Untitled/View/AlarmAddView.swift
@@ -8,18 +8,7 @@
 import UIKit
 
 class alarmAddView: UIView {
-    
-    func titleLabel() -> UILabel {
-        let label = UILabel()
-            
-        label.text = "알람 추가"
-        label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 18)
-        label.textAlignment = .center
-        
-        return label
-    }
-    
+
     func datePicker() -> UIDatePicker {
         let datePicker = UIDatePicker()
         
@@ -31,23 +20,5 @@ class alarmAddView: UIView {
         datePicker.tintColor = UIColor.applicationPointColorHighlightColor
         
         return datePicker
-    }
-    
-    func closeButton() -> UIButton {
-        let button = UIButton()
-        
-        button.setTitle("취소", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
-        
-        return button
-    }
-    
-    func saveButton() -> UIButton {
-        let button = UIButton()
-        
-        button.setTitle("저장", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 17.5)
-        
-        return button
     }
 }
