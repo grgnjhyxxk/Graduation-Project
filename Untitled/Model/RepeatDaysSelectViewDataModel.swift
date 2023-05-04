6f27128260cb8a15be6e79072c009aa3f9e962cf
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-05 04:46:17 +0900

    RepeatDaysSelectViewDataModel -> RepeatDaysSelectViewDataInitModel으로 변경 및 repeatDaysDataContractionText 추가

--- Untitled/Model/RepeatDaysSelectViewDataModel.swift
+++ Untitled/Model/RepeatDaysSelectViewDataModel.swift
@@ -7,35 +7,38 @@
 
 import UIKit
 
-var repeatDaysSelectList = [RepeatDaysSelectViewDataModel](repeating: RepeatDaysSelectViewDataModel(title: "", checkState: false), count: 7)
+var repeatDaysSelectList = [RepeatDaysSelectViewDataInitModel](repeating: RepeatDaysSelectViewDataInitModel(title: "", checkState: false), count: 7)
 
-
-struct RepeatDaysSelectViewDataModel {
+struct RepeatDaysSelectViewDataInitModel {
     let title: String
     var checkState: Bool
 }
 
 func repeatDaysSelectListInitFunction() {
     repeatDaysSelectList = [
-        RepeatDaysSelectViewDataModel(title: "월", checkState: false),
-        RepeatDaysSelectViewDataModel(title: "화", checkState: false),
-        RepeatDaysSelectViewDataModel(title: "수", checkState: false),
-        RepeatDaysSelectViewDataModel(title: "목", checkState: false),
-        RepeatDaysSelectViewDataModel(title: "금", checkState: false),
-        RepeatDaysSelectViewDataModel(title: "토", checkState: false),
-        RepeatDaysSelectViewDataModel(title: "일", checkState: false)
+        RepeatDaysSelectViewDataInitModel(title: "월", checkState: false),
+        RepeatDaysSelectViewDataInitModel(title: "화", checkState: false),
+        RepeatDaysSelectViewDataInitModel(title: "수", checkState: false),
+        RepeatDaysSelectViewDataInitModel(title: "목", checkState: false),
+        RepeatDaysSelectViewDataInitModel(title: "금", checkState: false),
+        RepeatDaysSelectViewDataInitModel(title: "토", checkState: false),
+        RepeatDaysSelectViewDataInitModel(title: "일", checkState: false)
     ]
 }
 
 func repeatDaysDataContraction(data: String) -> String {
     switch data {
     case "월 화 수 목 금":
+        repeatDaysDataContractionText = "주중"
         return "주중"
     case "월 화 수 목 금 토 일":
+        repeatDaysDataContractionText = "매일"
         return "매일"
     case "토 일":
+        repeatDaysDataContractionText = "주말"
         return "주말"
     default:
+        repeatDaysDataContractionText = "\(data)"
         return data
     }
 }
