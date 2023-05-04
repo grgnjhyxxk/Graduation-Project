551a3158ee0652eadaaef08a036e02679eed470e
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-04 19:47:31 +0900

    isHidden 요소 추가

--- Untitled/Model/AlarmAddViewCellDataModel.swift
+++ Untitled/Model/AlarmAddViewCellDataModel.swift
@@ -9,11 +9,12 @@ import UIKit
 
 struct AlarmAddViewCellDataModel {
     let title: String
+    let isHidden: Bool
 }
 
 let AlarmAddViewCellData = [
-    AlarmAddViewCellDataModel(title: "반복"),
-    AlarmAddViewCellDataModel(title: "레이블"),
-    AlarmAddViewCellDataModel(title: "사용자"),
-    AlarmAddViewCellDataModel(title: "다시 알림")
+    AlarmAddViewCellDataModel(title: "반복", isHidden: false),
+    AlarmAddViewCellDataModel(title: "레이블", isHidden: false),
+    AlarmAddViewCellDataModel(title: "사용자", isHidden: false),
+    AlarmAddViewCellDataModel(title: "다시 알림", isHidden: false)
 ]
