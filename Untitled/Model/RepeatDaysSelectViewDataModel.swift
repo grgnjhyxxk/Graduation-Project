e885f9747cc567b608100257a50b885cee11b9b9
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-04 19:47:10 +0900

    Model 및 초기화 함수 추가

--- Untitled/Model/RepeatDaysSelectViewDataModel.swift
+++ Untitled/Model/RepeatDaysSelectViewDataModel.swift
@@ -5,4 +5,21 @@
 //  Created by Jaehyeok Lim on 2023/05/04.
 //
 
-import Foundation
+import UIKit
+
+var repeatDaysSelectList = [RepeatDaysSelectViewDataModel]()
+
+struct RepeatDaysSelectViewDataModel {
+    let title: String
+    var checkState: Bool
+}
+
+func repeatDaysSelectListInitFunction() {
+    repeatDaysSelectList.append(RepeatDaysSelectViewDataModel(title: "월", checkState: false))
+    repeatDaysSelectList.append(RepeatDaysSelectViewDataModel(title: "화", checkState: false))
+    repeatDaysSelectList.append(RepeatDaysSelectViewDataModel(title: "수", checkState: false))
+    repeatDaysSelectList.append(RepeatDaysSelectViewDataModel(title: "목", checkState: false))
+    repeatDaysSelectList.append(RepeatDaysSelectViewDataModel(title: "금", checkState: false))
+    repeatDaysSelectList.append(RepeatDaysSelectViewDataModel(title: "토", checkState: false))
+    repeatDaysSelectList.append(RepeatDaysSelectViewDataModel(title: "일", checkState: false))
+}
