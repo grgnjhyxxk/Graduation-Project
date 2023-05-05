a62c0b1bd30902738b561d0d51fe542c8402851a
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-05 19:57:27 +0900

    InitView 대거 삭제

--- Untitled/Controller/InitViewController.swift
+++ Untitled/Controller/InitViewController.swift
@@ -14,14 +14,6 @@ class initViewController: UIViewController {
     
     var uiViewList: [UIView] = [] 
     
-    let mainTextLabel = initView().mainTextLabel()
-    let subTextLabel = initView().subTextLabel()
-    let initViewRingShapeLayer = initView().initViewRingShapeLayer()
-    let subTextLabelInInitViewRingShapeLayer = initView().subTextLabelInInitViewRingShapeLayer()
-    let loginButton = initView().loginButton()
-    let findIdPasswordTextLabel = initView().findIdPasswordTextLabel()
-    let signUpTextLabel = initView().signUpTextLabel()
-    
     func isLoggedIn() -> UIViewController {
         if isLoggedInBool {
             let customTabBarController = CustomTabBarController()
@@ -36,65 +28,21 @@ class initViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setSameBackgroundColor(firstAlpha: 0.57, secondAlpha: 0.57)
         addSubview()
         viewLayout()
-        setSameBackgroundColor(firstAlpha: 0.57, secondAlpha: 0.57)
-        action()
     }
     
     private func viewLayout() {
-        view.backgroundColor = UIColor.white
-        
-        let center = CGPoint(x: view.bounds.midX-100, y: view.bounds.midY-100)
-        initViewRingShapeLayer.position = center
-        
-        mainTextLabel.snp.makeConstraints { make in
-            make.top.equalTo(view).offset(90)
-            make.leading.equalTo(view).offset(27)
-        }
-        
-        subTextLabel.snp.makeConstraints { make in
-            make.top.equalTo(mainTextLabel.snp.bottom).offset(22)
-            make.leading.equalTo(mainTextLabel)
-        }
-        
-        subTextLabelInInitViewRingShapeLayer.snp.makeConstraints { make in
-            make.center.equalTo(view)
-        }
-        
-        loginButton.snp.makeConstraints { make in
-            make.top.equalTo(view).offset(630)
-            make.leading.equalTo(view).offset(70)
-            make.trailing.equalTo(view).offset(-70)
-            make.height.equalTo(50)
-        }
-        
-        findIdPasswordTextLabel.snp.makeConstraints { make in
-            make.top.equalTo(loginButton.snp.bottom).offset(10)
-            make.leading.equalTo(loginButton.snp.leading).offset(0.5)
-        }
-        
-        signUpTextLabel.snp.makeConstraints { make in
-            make.top.equalTo(loginButton.snp.bottom).offset(10)
-            make.trailing.equalTo(loginButton.snp.trailing).offset(-0.5)
-        }
+        view.backgroundColor = UIColor.baseViewBackgroundColor
     }
     
     private func addSubview() {
-        uiViewList = [mainTextLabel, subTextLabel, subTextLabelInInitViewRingShapeLayer, loginButton, findIdPasswordTextLabel, signUpTextLabel]
+        uiViewList = []
         
         for uiView in uiViewList {
             view.addSubview(uiView)
-            
         }
-        
-        view.layer.addSublayer(initViewRingShapeLayer)
-    }
-    
-    private func action() {
-        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
-        loginButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
-        loginButton.addTarget(self, action: #selector(buttonTouchUp), for: [.touchUpInside, .touchUpOutside])
     }
 
     @objc func loginButtonTapped(_ sender: UIButton) {
@@ -104,14 +52,5 @@ class initViewController: UIViewController {
         tabBarController.modalPresentationStyle = .fullScreen
         
         present(tabBarController, animated: true)
-    }
-    
-
-    @objc func buttonTouchDown(_ sender: UIButton) {
-        sender.backgroundColor = UIColor.applicationPointColorHighlightColor
-    }
-
-    @objc func buttonTouchUp(_ sender: UIButton) {
-        sender.backgroundColor = UIColor.applicationPointColor
     }
 }
--- Untitled/View/InitView.swift
+++ Untitled/View/InitView.swift
@@ -9,56 +9,10 @@ import UIKit
 
 class initView: UIView {
     
-    func mainTextLabel() -> UIView {
-        let label = UILabel()
-        
-        label.text = "더 편리하고\n가볍게"
-        label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 40)
-        label.numberOfLines = 0
-        
-        return label
-    }
-    
-    func subTextLabel() -> UIView {
-        let label = UILabel()
-        
-        label.text = "저희가 삶의 원동력이 되어드릴게요."
-        label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 15)
-        
-        return label
-    }
-    
-    func initViewRingShapeLayer() -> CAShapeLayer {
-        let radius: CGFloat = 50
-        let lineWidth: CGFloat = 11
-
-        let path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: radius * 4, height: radius * 4))
-
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = path.cgPath
-        shapeLayer.lineWidth = lineWidth
-        shapeLayer.strokeColor = UIColor.applicationPointColor.cgColor
-        shapeLayer.fillColor = UIColor.clear.cgColor
-
-        return shapeLayer
-    }
-    
-    func subTextLabelInInitViewRingShapeLayer() -> UIView {
-        let label = UILabel()
-        
-        label.text = "삶의 원동력, 하루의 활력소"
-        label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 13)
-        
-        return label
-    }
-    
     func loginButton() -> UIButton {
         let button = UIButton()
         
-        button.backgroundColor = UIColor.applicationPointColor
+        button.backgroundColor = UIColor.white
         button.setTitle("기존 계정으로 로그인", for: .normal)
         button.setTitleColor(UIColor.white, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18.5)
