fc328cf83c59865e43b72b64945feb00b8570751
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-07 22:56:46 +0900

    id/pwd Field 추가

--- Untitled/View/InitView.swift
+++ Untitled/View/InitView.swift
@@ -9,14 +9,47 @@ import UIKit
 
 class initView: UIView {
     
+    func idInputTextField() -> UITextField {
+        let textField = UITextField()
+        
+        textField.placeholder = "아이디"
+        textField.textColor = UIColor.white
+        textField.backgroundColor = UIColor.white.withAlphaComponent(0.07)
+        textField.layer.cornerRadius = 5
+        textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 10.0, height: 0.0))
+        textField.leftViewMode = .always
+        textField.layer.borderWidth = 0.5
+        textField.layer.borderColor = UIColor.black.withAlphaComponent(0.1).cgColor
+        
+        return textField
+    }
+    
+    func PasswordInputTextField() -> UITextField {
+        let textField = UITextField()
+        
+        textField.placeholder = "비밀번호"
+        textField.textColor = UIColor.white
+        textField.backgroundColor = UIColor.white.withAlphaComponent(0.07)
+        textField.layer.cornerRadius = 5
+        textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 10.0, height: 0.0))
+        textField.leftViewMode = .always
+        textField.layer.borderWidth = 0.5
+        textField.layer.borderColor = UIColor.black.withAlphaComponent(0.1).cgColor
+        
+        return textField
+    }
+    
     func loginButton() -> UIButton {
         let button = UIButton()
         
-        button.backgroundColor = UIColor.white
-        button.setTitle("기존 계정으로 로그인", for: .normal)
-        button.setTitleColor(UIColor.white, for: .normal)
+        button.backgroundColor = UIColor.black.withAlphaComponent(0.25)
+        button.setTitle("로그인", for: .normal)
+        button.setTitleColor(UIColor.white.withAlphaComponent(0.7), for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18.5)
-        button.layer.cornerRadius = 7.5
+        button.titleLabel?.textAlignment = .center
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.black.withAlphaComponent(0.07).cgColor
         
         return button
     }
