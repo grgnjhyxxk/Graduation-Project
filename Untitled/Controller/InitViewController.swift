55124e1bb5c97af6045fd07e1b69e60287b4c3d7
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-08 23:28:39 +0900

    commonView 추가 및 위치 조정

--- Untitled/Controller/InitViewController.swift
+++ Untitled/Controller/InitViewController.swift
@@ -13,49 +13,72 @@ class initViewController: UIViewController {
     var isLoggedInBool = false
     
     var uiViewList: [UIView] = []
-    
+    var commonViewList: [UIView] = []
+
     let idInputTextFelid = initView().idInputTextField()
     let passwordTextFelid = initView().PasswordInputTextField()
     let loginButton = initView().loginButton()
+    
+    let commonUiView = commonView().commonUiView(backgroundColor: UIColor.layerViewBackgroundColor ?? UIColor.black.withAlphaComponent(0.07), borderWidth: 0, borderColor: UIColor.clear, cornerRadius: 15)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setSameBackgroundColor(firstAlpha: 0.57, secondAlpha: 0.57)
         addSubview()
         viewLayout()
+        addOnCommonUiView()
+        commonUIViewLayout()
         actionFunction()
     }
     
     private func viewLayout() {
         view.backgroundColor = UIColor.baseViewBackgroundColor
         
+        commonUiView.snp.makeConstraints { make in
+//            make.top.equalTo(view).offset(330)
+//            make.leading.equalTo(25)
+//            make.trailing.equalTo(-25)
+//            make.height.equalTo(180)
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func addSubview() {
+        uiViewList = [commonUiView]
+        
+        for uiView in uiViewList {
+            view.addSubview(uiView)
+        }
+    }
+    
+    private func commonUIViewLayout() {
         idInputTextFelid.snp.makeConstraints { make in
-            make.top.equalTo(300)
-            make.leading.equalTo(40)
-            make.trailing.equalTo(-40)
+            make.top.equalTo(355)
+            make.leading.equalTo(30)
+            make.trailing.equalTo(-30)
             make.height.equalTo(40)
         }
         
         passwordTextFelid.snp.makeConstraints { make in
             make.top.equalTo(idInputTextFelid.snp.bottom).offset(5)
-            make.leading.equalTo(40)
-            make.trailing.equalTo(-40)
+            make.leading.equalTo(30)
+            make.trailing.equalTo(-30)
             make.height.equalTo(40)
         }
         
         loginButton.snp.makeConstraints { make in
             make.top.equalTo(passwordTextFelid.snp.bottom).offset(10)
-            make.leading.equalTo(40)
-            make.trailing.equalTo(-40)
+            make.leading.equalTo(30)
+            make.trailing.equalTo(-30)
             make.height.equalTo(35)
         }
     }
     
-    private func addSubview() {
-        uiViewList = [idInputTextFelid, passwordTextFelid, loginButton]
+    private func addOnCommonUiView() {
+        commonViewList = [idInputTextFelid, passwordTextFelid, loginButton]
         
-        for uiView in uiViewList {
-            view.addSubview(uiView)
+        for uiView in commonViewList {
+            commonUiView.addSubview(uiView)
         }
     }
     
