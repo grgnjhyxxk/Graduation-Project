48e35ad98411987bfc23d1a65179cb377f8abfba
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-09 04:15:15 +0900

    기본 뷰 구성

--- Untitled/Controller/RegisterViewController.swift
+++ Untitled/Controller/RegisterViewController.swift
@@ -5,4 +5,51 @@
 //  Created by Jaehyeok Lim on 2023/05/09.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class RegisterViewController: UIViewController {
+    
+    var uiViewList: [UIView] = []
+    var commonViewList: [UIView] = []
+
+    let commonUiView = commonView().commonUiView(backgroundColor: UIColor.layerViewBackgroundColor ?? UIColor.black.withAlphaComponent(0.07), borderWidth: 0, borderColor: UIColor.clear, cornerRadius: 15)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setSameBackgroundColor(firstAlpha: 0.57, secondAlpha: 0.57)
+        addSubview()
+        viewLayout()
+        addOnCommonUiView()
+        commonUIViewLayout()
+    }
+    
+    private func viewLayout() {
+        view.backgroundColor = UIColor.baseViewBackgroundColor
+        
+        commonUiView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func addSubview() {
+        uiViewList = [commonUiView]
+        
+        for uiView in uiViewList {
+            view.addSubview(uiView)
+        }
+    }
+    
+    private func commonUIViewLayout() {
+        
+    }
+    
+    private func addOnCommonUiView() {
+        commonViewList = []
+        
+        for uiView in commonViewList {
+            commonUiView.addSubview(uiView)
+        }
+    }
+}
+  
