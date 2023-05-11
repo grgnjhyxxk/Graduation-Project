418e4d1ce748ab769f46b5a63093cd6e00de5e89
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-11 23:42:19 +0900

    roundingImage 삭제 및 rounding Button 수정

--- Untitled/View/CommonView.swift
+++ Untitled/View/CommonView.swift
@@ -11,32 +11,14 @@ private let userProfileImageString = "TemporaryUserProfilePicture"
 
 class CommonView: UIView {
     
-    func roundingImage(string: String) -> UIImage {
-        let originalImage = UIImage(named: "\(string)")
-
-        // 이미지 크기와 동일한 새로운 이미지 생성
-        let imageFrame = CGRect(x: 0, y: 0, width: originalImage!.size.width, height: originalImage!.size.height)
-        UIGraphicsBeginImageContextWithOptions(imageFrame.size, false, 0.0)
-        let context = UIGraphicsGetCurrentContext()
-        context?.saveGState()
-        UIBezierPath(roundedRect: imageFrame, cornerRadius: imageFrame.size.width/2.0).addClip()
-        originalImage?.draw(in: imageFrame)
-        context?.restoreGState()
-        let roundedImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        return roundedImage
-    }
-    
     func roundingButton() -> UIView {
-    
-        let image = roundingImage(string: userProfileImageString)
         
         let button = UIButton()
-        button.setImage(image, for: .normal)
+        button.backgroundColor = UIColor.appSubBackgroundColor
         button.contentVerticalAlignment = .fill
         button.contentHorizontalAlignment = .fill
         button.layer.cornerRadius = 22.5
+        button.clipsToBounds = true
         button.appButtonCommonStyle()
         
         return button
