85e59117bdfc1565bc1390e5ec86d1eaa79b16b7
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-12 01:46:38 +0900

    hideKeyboardWhenTappedAround 추가

--- Untitled/Extension/Extension.swift
+++ Untitled/Extension/Extension.swift
@@ -7,6 +7,19 @@
 
 import UIKit
 
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
 extension UIView {
     func shadowLayer() {
         layer.shadowColor = UIColor.black.cgColor
