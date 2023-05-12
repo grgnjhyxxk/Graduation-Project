51491cae4d810a75609aad2a11d13b2111bd1265
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-12 22:43:59 +0900

    titleButton 추가

--- Untitled/View/HomeView.swift
+++ Untitled/View/HomeView.swift
@@ -18,4 +18,17 @@ class HomeView: UIView {
         
         return imageView
     }
+    
+    func titleButton(text: String) -> UIButton {
+        let button = UIButton()
+        
+        button.setTitle("\(text)", for: .normal)
+        button.backgroundColor = UIColor.appSubBackgroundColor
+        button.titleLabel?.font = UIFont(name: "NotoSansKR-Regular", size: 14)
+        button.setTitleColor(UIColor.appTextColor, for: .normal)
+        button.layer.cornerRadius = 10
+        button.titleLabel?.textAlignment = .center
+        
+        return button
+    }
 }
