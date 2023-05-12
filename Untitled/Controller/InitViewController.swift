497677282237a1e7d40c29ad79a0e8b4fb2a57ce
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-12 22:49:17 +0900

    autoLoginButton,Label 과 saveIdButton,Label, findButton 추가

--- Untitled/Controller/InitViewController.swift
+++ Untitled/Controller/InitViewController.swift
@@ -19,15 +19,20 @@ class InitViewController: UIViewController {
     
     let mainTextLabel = InitView().mainTextLabel()
     let subTextLabel = InitView().subTextLabel()
-    let registerLabel = InitView().registerLabel()
-    
+    let registerLabel = InitView().serviceLabel(text: "회원이 아니신가요?")
+    let autoLoginLabel = CommonView().commonTextLabel(labelText: "자동로그인", size: 13)
+    let saveIdLabel = CommonView().commonTextLabel(labelText: "아이디 저장", size: 13)
+
     let idInputTextField = InitView().idInputTextField()
     let PasswordInputTextField = InitView().PasswordInputTextField()
     
     let passwordVisibilityButton = InitView().passwordVisibilityButton()
     let loginButton = InitView().loginButton()
-    let registerButton = InitView().registerButton()
-    
+    let findButton = InitView().serviceButton(text: "아이디/비밀번호 찾기")
+    let registerButton = InitView().serviceButton(text: "가입하기")
+    let autoLoginButton = InitView().toggleButton()
+    let saveIdButton = InitView().toggleButton()
+
     let commonUiView = CommonView().commonUiView(backgroundColor: UIColor.clear, borderWidth: 0, borderColor: UIColor.clear, cornerRadius: 15)
     
     override func viewDidLoad() {
@@ -93,15 +98,42 @@ class InitViewController: UIViewController {
             make.trailing.equalTo(PasswordInputTextField).offset(-8.5)
         }
         
+        autoLoginButton.snp.makeConstraints { make in
+            make.top.equalTo(PasswordInputTextField.snp.bottom).offset(10)
+            make.leading.equalTo(25)
+            make.size.equalTo(CGSize(width: 30, height: 30))
+        }
+        
+        autoLoginLabel.snp.makeConstraints { make in
+            make.top.equalTo(autoLoginButton).offset(4.5)
+            make.leading.equalTo(autoLoginButton.snp.trailing).offset(0)
+        }
+        
+        saveIdButton.snp.makeConstraints { make in
+            make.top.equalTo(autoLoginButton)
+            make.leading.equalTo(autoLoginLabel.snp.trailing).offset(10)
+            make.size.equalTo(CGSize(width: 30, height: 30))
+        }
+        
+        saveIdLabel.snp.makeConstraints { make in
+            make.top.equalTo(saveIdButton).offset(4.5)
+            make.leading.equalTo(saveIdButton.snp.trailing).offset(0)
+        }
+        
+        findButton.snp.makeConstraints { make in
+            make.top.equalTo(saveIdButton).offset(-2)
+            make.trailing.equalTo(PasswordInputTextField)
+        }
+        
         loginButton.snp.makeConstraints { make in
-            make.top.equalTo(PasswordInputTextField.snp.bottom).offset(30)
+            make.top.equalTo(autoLoginButton.snp.bottom).offset(30)
             make.leading.equalTo(30)
             make.trailing.equalTo(-30)
             make.height.equalTo(35)
         }
         
         registerLabel.snp.makeConstraints { make in
-            make.top.equalTo(loginButton.snp.bottom).offset(330)
+            make.top.equalTo(loginButton.snp.bottom).offset(300)
             make.leading.equalTo(116)
         }
         
@@ -109,10 +141,13 @@ class InitViewController: UIViewController {
             make.top.equalTo(registerLabel).offset(-6.8)
             make.leading.equalTo(registerLabel.snp.trailing).offset(5)
         }
+        
+        findButton.titleLabel?.textAlignment = .right
+        findButton.titleLabel?.font = UIFont(name: "NotoSansKR-Bold", size: 13)
     }
     
     private func addOnCommonUiView() {
-        commonViewList = [mainImageView, mainTextLabel, subTextLabel, idInputTextField, PasswordInputTextField, passwordVisibilityButton, loginButton, registerLabel, registerButton]
+        commonViewList = [mainImageView, mainTextLabel, subTextLabel, idInputTextField, PasswordInputTextField, passwordVisibilityButton, autoLoginButton, autoLoginLabel, saveIdButton, saveIdLabel, findButton, loginButton, registerLabel, registerButton]
         
         for uiView in commonViewList {
             commonUiView.addSubview(uiView)
@@ -121,10 +156,7 @@ class InitViewController: UIViewController {
     
     func isLoggedIn() -> UIViewController {
         if isLoggedInBool {
-            let customTabBarController = CustomTabBarController()
-            let navigationController = UINavigationController(rootViewController: customTabBarController)
-            
-            return navigationController
+            return CustomTabBarController()
             
         } else {
             return InitViewController()
@@ -135,6 +167,8 @@ class InitViewController: UIViewController {
         loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
         passwordVisibilityButton.addTarget(self, action: #selector(passwordVisibilityButtonTapped), for: .touchUpInside)
         registerButton.addTarget(self, action: #selector(registerButtonAction), for: .touchUpInside)
+        autoLoginButton.addTarget(self, action: #selector(autoLoginButtonAction), for: .touchUpInside)
+        saveIdButton.addTarget(self, action: #selector(saveIdButtonAction), for: .touchUpInside)
     }
     
     private func delegateFunction() {
@@ -161,6 +195,22 @@ class InitViewController: UIViewController {
         let navigationController = UINavigationController(rootViewController: rootViewController)
         
         present(navigationController, animated: true, completion: nil)
+    }
+    
+    @objc func autoLoginButtonAction(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        let imageName = sender.isSelected ? "checkmark.circle.fill" : "circle"
+        let tintColor = sender.isSelected ? UIColor.appPointColor : UIColor.placeholderText
+        sender.setImage(UIImage(systemName: imageName), for: .normal)
+        sender.tintColor = tintColor
+    }
+    
+    @objc func saveIdButtonAction(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        let imageName = sender.isSelected ? "checkmark.circle.fill" : "circle"
+        let tintColor = sender.isSelected ? UIColor.appPointColor : UIColor.placeholderText
+        sender.setImage(UIImage(systemName: imageName), for: .normal)
+        sender.tintColor = tintColor
     }
 }
 
