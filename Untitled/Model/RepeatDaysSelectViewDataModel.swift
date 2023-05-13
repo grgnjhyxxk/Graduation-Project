04d4f11238b95e5697334dea3fc5790c861f369d
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-14 00:21:15 +0900

    repeatDaysDataReverseContraction 케이스 예외가 있어 수정

--- Untitled/Model/RepeatDaysSelectViewDataModel.swift
+++ Untitled/Model/RepeatDaysSelectViewDataModel.swift
@@ -30,21 +30,23 @@ func repeatDaysDataContraction(data: String) -> String {
     switch data {
     case "월 화 수 목 금":
         repeatDaysDataContractionText = "주중"
+        return "주중"
     case "월 화 수 목 금 토 일":
         repeatDaysDataContractionText = "매일"
+        return "매일"
     case "토 일":
         repeatDaysDataContractionText = "주말"
+        return "주말"
     default:
+        repeatDaysDataContractionText = "\(data)"
         return data
     }
-    print("반복 주기의 문자열 변환 과정이 정상적으로 처리되었습니다.")
-    return repeatDaysDataContractionText
 }
 
-func repeatDaysDataReverseContraction(data: Array<AlarmViewCellDataModel>, index: Int) {
+func repeatDaysDataReverseContraction(index: Int) {
     repeatDaysSelectListInitFunction()
     
-    switch data[index].repeatDays{
+    switch alarmViewCellDataList[index].repeatDays{
     case "주중":
         repeatDaysSelectList[0].checkState = true
         repeatDaysSelectList[1].checkState = true
@@ -63,14 +65,27 @@ func repeatDaysDataReverseContraction(data: Array<AlarmViewCellDataModel>, index
         repeatDaysSelectList[5].checkState = true
         repeatDaysSelectList[6].checkState = true
     default:
-        repeatDaysSelectList[0].checkState = false
-        repeatDaysSelectList[1].checkState = false
-        repeatDaysSelectList[2].checkState = false
-        repeatDaysSelectList[3].checkState = false
-        repeatDaysSelectList[4].checkState = false
-        repeatDaysSelectList[5].checkState = false
-        repeatDaysSelectList[6].checkState = false
-        break
+        let days = alarmViewCellDataList[index].repeatDays.split(separator: " ")
+        for day in days {
+            switch day {
+            case "월":
+                repeatDaysSelectList[0].checkState = true
+            case "화":
+                repeatDaysSelectList[1].checkState = true
+            case "수":
+                repeatDaysSelectList[2].checkState = true
+            case "목":
+                repeatDaysSelectList[3].checkState = true
+            case "금":
+                repeatDaysSelectList[4].checkState = true
+            case "토":
+                repeatDaysSelectList[5].checkState = true
+            case "일":
+                repeatDaysSelectList[6].checkState = true
+            default:
+                break
+            }
+        }
     }
     print("알람 편집 중 반복 주기 리버스 과정이 정상적으로 처리되었습니다.")
 }
