2e0702d1c748bdb74f617c45abc7985ec9086153
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-15 02:48:54 +0900

    초기 세팅

--- Untitled/Controller/User/UserProfileViewController.swift
+++ Untitled/Controller/User/UserProfileViewController.swift
@@ -5,4 +5,64 @@
 //  Created by Jaehyeok Lim on 2023/05/15.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class UserProfileViewController: UIViewController {
+    
+    var uiViewList: [UIView] = []
+    var commonViewList: [UIView] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func viewLayout() {
+        view.backgroundColor = UIColor.appMainBackgroundColor
+        
+    }
+    
+    private func addSubview() {
+        uiViewList = []
+        
+        for uiView in uiViewList {
+            view.addSubview(uiView)
+        }
+    }
+    
+    private func commonUIViewLayout() {
+        
+    }
+    
+    private func addOnCommonUiView() {
+        commonViewList = []
+        
+        for uiView in commonViewList {
+            
+        }
+    }
+    
+    private func navigationControllerLayout() {
+        navigationItem.title = "프로필"
+        
+        let cancelButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelButtonAction))
+        cancelButton.tintColor = UIColor.appTextColor
+        navigationItem.leftBarButtonItem = cancelButton
+        
+        let saveButton = UIBarButtonItem(title: "저장", style: .done, target: self, action: #selector(saveButtonAction))
+        saveButton.tintColor = UIColor.appPointColor
+        navigationItem.rightBarButtonItem = saveButton
+        
+        navigationController?.navigationBar.barTintColor = .black
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.appTextColor!]
+    }
+    
+    @objc func cancelButtonAction(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @objc func saveButtonAction(_ sender: UIBarButtonItem) {
+        
+    }
+}
