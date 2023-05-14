22bd71e5b8a266a66623bed82f171c3f688c911e
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-15 02:04:19 +0900

    Loading Session 추가

--- Untitled/View/LoadingView.swift
+++ Untitled/View/LoadingView.swift
@@ -5,4 +5,30 @@
 //  Created by Jaehyeok Lim on 2023/05/15.
 //
 
-import Foundation
+import UIKit
+
+var loadingView: UIView?
+
+func showLoadingScreen() {
+    let screenSize = UIScreen.main.bounds.size
+    loadingView = UIView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
+    loadingView?.backgroundColor = UIColor(white: 0, alpha: 0.5)
+    
+    let activityIndicator = UIActivityIndicatorView(style: .large)
+    activityIndicator.center = loadingView!.center
+    activityIndicator.startAnimating()
+    
+    loadingView?.addSubview(activityIndicator)
+        
+    guard let keyWindow = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else {
+        return
+    }
+    
+    keyWindow.addSubview(loadingView!)
+}
+
+func hideLoadingScreen() {
+    // 로딩 화면을 숨기는 코드 작성
+    loadingView?.removeFromSuperview()
+    loadingView = nil
+}
