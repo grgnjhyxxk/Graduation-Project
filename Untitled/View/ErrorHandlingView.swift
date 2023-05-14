d09f228b403ed11bcef9fb8f1fe48ade1187a9d2
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-15 02:42:24 +0900

    Networking 중 error 발생 시 alert 생성 함수 생성

--- Untitled/View/ErrorHandlingView.swift
+++ Untitled/View/ErrorHandlingView.swift
@@ -5,4 +5,13 @@
 //  Created by Jaehyeok Lim on 2023/05/15.
 //
 
-import Foundation
+import UIKit
+
+func networkErrorHandlingAlert() {
+    if let topWindow = UIApplication.shared.windows.last {
+        let alert = UIAlertController(title: "네트워크 에러", message: "현재 서버와의 연결이 불안정합니다.\n잠시 후에 시도해 주시기 바랍니다.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(okAction)
+        topWindow.rootViewController?.present(alert, animated: true, completion: nil)
+    }
+}
