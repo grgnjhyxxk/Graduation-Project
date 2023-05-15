f3e07f63bc6d296f10504315ec9145e88603ff28
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-16 05:25:20 +0900

    코드 수정

--- Untitled/Common/SceneDelegate.swift
+++ Untitled/Common/SceneDelegate.swift
@@ -20,6 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = LoginViewController().isLoggedIn()
+//        window?.rootViewController = UserViewContoller()
         window?.makeKeyAndVisible()
     }
 
