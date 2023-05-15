91facd18a56716a9e36d13bbe166b31e60cb84a8
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-16 05:27:17 +0900

    로그아웃 버튼 추가

--- Untitled/Controller/User/UserViewContoller.swift
+++ Untitled/Controller/User/UserViewContoller.swift
@@ -13,7 +13,16 @@ class UserViewContoller: UIViewController {
     var uiViewList: [UIView] = []
     var commonViewList: [UIView] = []
     
+    let commonUiView = CommonView().commonUiView(backgroundColor: UIColor.appSubBackgroundColor!, borderWidth: 0, borderColor: UIColor.clear, cornerRadius: 15)
+    
     let userPofileImage = RegisterView().selectUserProfileImageView()
+
+    let mainTitleLabel = CommonView().commonTextLabel(labelText: "임재혁 님\n오늘도 와주셨네요!", size: 20)
+    let pillViewTtileLabel = CommonView().commonTextLabel(labelText: "내 영양제", size: 20)
+
+    let profileEditButton = InitView().serviceButton(text: "프로필 수정")
+    let plusbutton = AlarmView().plusButton()
+    let logoutButton = InitView().serviceButton(text: "로그아웃")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +31,7 @@ class UserViewContoller: UIViewController {
         addOnCommonUiView()
         commonUIViewLayout()
         navigationControllerLayout()
+        actionFunction()
     }
     
     private func viewLayout() {
@@ -30,14 +40,39 @@ class UserViewContoller: UIViewController {
         userPofileImage.snp.makeConstraints { make in
             make.top.equalTo(130)
             make.trailing.equalTo(-20)
-            make.size.equalTo(CGSize(width: 80, height: 80))
+            make.size.equalTo(CGSize(width: 70, height: 70))
         }
         
-        userPofileImage.layer.cornerRadius = 40
+        mainTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(userPofileImage).offset(7)
+            make.leading.equalTo(20)
+        }
+        
+        profileEditButton.snp.makeConstraints { make in
+            make.top.equalTo(mainTitleLabel.snp.bottom)
+            make.leading.equalTo(mainTitleLabel)
+        }
+        
+        commonUiView.snp.makeConstraints { make in
+            make.top.equalTo(profileEditButton.snp.bottom).offset(30)
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.height.equalTo(150)
+        }
+        
+        logoutButton.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.bottom).offset(-57.8)
+            make.centerX.equalTo(view)
+        }
+        
+        userPofileImage.layer.cornerRadius = 35
+        mainTitleLabel.attributedLabel(text: "임재혁")
+        mainTitleLabel.font = UIFont(name: "Roboto-Bold", size: 25)
+        logoutButton.setTitleColor(UIColor.systemRed, for: .normal)
     }
     
     private func addSubview() {
-        uiViewList = [userPofileImage]
+        uiViewList = [userPofileImage, mainTitleLabel, profileEditButton, commonUiView, logoutButton]
         
         for uiView in uiViewList {
             view.addSubview(uiView)
@@ -45,14 +80,25 @@ class UserViewContoller: UIViewController {
     }
     
     private func commonUIViewLayout() {
+        plusbutton.snp.makeConstraints { make in
+            make.top.equalTo(15)
+            make.trailing.equalTo(-15)
+            make.size.equalTo(CGSize(width: 23, height: 23))
+        }
         
+        pillViewTtileLabel.snp.makeConstraints { make in
+            make.top.equalTo(15)
+            make.leading.equalTo(15)
+        }
+        
+        pillViewTtileLabel.font = UIFont(name: "Roboto-Bold", size: 15)
     }
     
     private func addOnCommonUiView() {
-        commonViewList = []
+        commonViewList = [plusbutton, pillViewTtileLabel]
         
         for uiView in commonViewList {
-            
+            commonUiView.addSubview(uiView)
         }
     }
     
@@ -72,6 +118,10 @@ class UserViewContoller: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.appTextColor!]
     }
     
+    private func actionFunction() {
+        logoutButton.addTarget(self, action: #selector(logoutButtonAction), for: .touchUpInside)
+    }
+    
     @objc func cancelButtonAction(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
@@ -79,5 +129,15 @@ class UserViewContoller: UIViewController {
     @objc func saveButtonAction(_ sender: UIBarButtonItem) {
         
     }
+    
+    @objc func logoutButtonAction(_ sender: UIButton) {
+        showLoadingScreen()
+        print("로그아웃버튼 isSelected")
+        NotificationCenter.default.post(name: NSNotification.Name("LogoutNotification"), object: nil)
+        presentingViewController?.presentingViewController?.presentedViewController?.dismiss(animated: true)        
+
+        hideLoadingScreen()
+    }
+
 }
 
