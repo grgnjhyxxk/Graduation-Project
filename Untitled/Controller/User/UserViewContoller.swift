7392e335baed8cab3077f45aaf2e2f5d771cf41a
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-17 21:36:51 +0900

    레이아웃 조정

--- Untitled/Controller/User/UserViewContoller.swift
+++ Untitled/Controller/User/UserViewContoller.swift
@@ -9,16 +9,18 @@ import UIKit
 import SnapKit
 
 class UserViewContoller: UIViewController {
+//    var serialNumber = String()
     
     var uiViewList: [UIView] = []
     var commonViewList: [UIView] = []
     
-    let commonUiView = CommonView().commonUiView(backgroundColor: UIColor.appSubBackgroundColor!, borderWidth: 0, borderColor: UIColor.clear, cornerRadius: 15)
-    
+    let commonUiView = CommonView().commonUiView(backgroundColor: UIColor.appMainBackgroundColor!, borderWidth: 0, borderColor: UIColor.clear, cornerRadius: 20)
+
     let userPofileImage = RegisterView().selectUserProfileImageView()
 
     let mainTitleLabel = CommonView().commonTextLabel(labelText: "임재혁 님\n오늘도 와주셨네요!", size: 20)
     let pillViewTtileLabel = CommonView().commonTextLabel(labelText: "내 영양제", size: 20)
+//    let serialNumber = CommonView().commonTextLabel(labelText: "", size: <#T##CGFloat#>)
 
     let profileEditButton = InitView().serviceButton(text: "프로필 수정")
     let plusbutton = AlarmView().plusButton()
@@ -37,15 +39,19 @@ class UserViewContoller: UIViewController {
     private func viewLayout() {
         view.backgroundColor = UIColor.appMainBackgroundColor
         
-        userPofileImage.snp.makeConstraints { make in
-            make.top.equalTo(130)
-            make.trailing.equalTo(-20)
-            make.size.equalTo(CGSize(width: 70, height: 70))
-        }
+        let dividerView = UIView(frame: CGRect(x: 0, y: (navigationController?.navigationBar.frame.maxY)! + 60, width: view.frame.width, height: 1))
+        dividerView.backgroundColor = UIColor.systemGray6
+        navigationController?.view.addSubview(dividerView)
         
         mainTitleLabel.snp.makeConstraints { make in
-            make.top.equalTo(userPofileImage).offset(7)
+            make.top.equalTo(130)
             make.leading.equalTo(20)
+        }
+        
+        userPofileImage.snp.makeConstraints { make in
+            make.top.equalTo(mainTitleLabel)
+            make.trailing.equalTo(-20)
+            make.size.equalTo(CGSize(width: 70, height: 70))
         }
         
         profileEditButton.snp.makeConstraints { make in
@@ -57,7 +63,7 @@ class UserViewContoller: UIViewController {
             make.top.equalTo(profileEditButton.snp.bottom).offset(30)
             make.leading.equalTo(20)
             make.trailing.equalTo(-20)
-            make.height.equalTo(150)
+            make.height.equalTo(120)
         }
         
         logoutButton.snp.makeConstraints { make in
@@ -65,10 +71,15 @@ class UserViewContoller: UIViewController {
             make.centerX.equalTo(view)
         }
         
+        commonUiView.subShadowLayer()
+        userPofileImage.backgroundColor = UIColor.appSubBackgroundColor
         userPofileImage.layer.cornerRadius = 35
         mainTitleLabel.attributedLabel(text: "임재혁")
         mainTitleLabel.font = UIFont(name: "Roboto-Bold", size: 25)
+        profileEditButton.titleLabel?.font = UIFont(name: "Roboto-Regular", size: 13)
+        profileEditButton.setTitleColor(UIColor.subTextColor, for: .normal)
         logoutButton.setTitleColor(UIColor.systemRed, for: .normal)
+        logoutButton.titleLabel?.font = UIFont(name: "Roboto-Regular", size: 13)
     }
     
     private func addSubview() {
@@ -105,7 +116,7 @@ class UserViewContoller: UIViewController {
     private func navigationControllerLayout() {
         navigationItem.title = "마이홈"
         
-        let cancelButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelButtonAction))
+        let cancelButton = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(cancelButtonAction))
         cancelButton.tintColor = UIColor.appTextColor
         navigationItem.leftBarButtonItem = cancelButton
         
@@ -120,6 +131,7 @@ class UserViewContoller: UIViewController {
     
     private func actionFunction() {
         logoutButton.addTarget(self, action: #selector(logoutButtonAction), for: .touchUpInside)
+        profileEditButton.addTarget(self, action: #selector(profileEditButtonAction), for: .touchUpInside)
     }
     
     @objc func cancelButtonAction(_ sender: UIBarButtonItem) {
@@ -130,12 +142,17 @@ class UserViewContoller: UIViewController {
         
     }
     
+    @objc func profileEditButtonAction(_ sender: UIButton) {
+        let rootViewControoler = UserProfileEditViewController()
+        show(rootViewControoler, sender: nil)
+    }
+    
     @objc func logoutButtonAction(_ sender: UIButton) {
         showLoadingScreen()
         print("로그아웃버튼 isSelected")
         NotificationCenter.default.post(name: NSNotification.Name("LogoutNotification"), object: nil)
-        presentingViewController?.presentingViewController?.presentedViewController?.dismiss(animated: true)        
-
+        presentingViewController?.presentingViewController?.dismiss(animated: true)
+        UserDefaults.standard.set(false, forKey: "auto")
         hideLoadingScreen()
     }
 
