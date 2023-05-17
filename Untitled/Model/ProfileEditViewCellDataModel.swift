8ef87fa8731632a6e20c8720d4a0120901e8dbda
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-17 21:34:37 +0900

    profileEditViewCellData 추가

--- Untitled/Model/ProfileEditViewCellDataModel.swift
+++ Untitled/Model/ProfileEditViewCellDataModel.swift
@@ -5,4 +5,16 @@
 //  Created by Jaehyeok Lim on 2023/05/16.
 //
 
-import Foundation
+import UIKit
+
+struct ProfileEditViewCellDataModel {
+    let title: String
+    let isHidden: Bool
+}
+
+let profileEditViewCellData = [
+    ProfileEditViewCellDataModel(title: "이름", isHidden: false),
+    ProfileEditViewCellDataModel(title: "생년월일", isHidden: false),
+    ProfileEditViewCellDataModel(title: "성별", isHidden: false),
+    ProfileEditViewCellDataModel(title: "기기번호", isHidden: false)
+]
