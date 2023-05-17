03b7a824db7c47b8dce032f8324954f2ccb1653e
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-17 21:33:12 +0900

    inital commit

---
+++ Untitled/Cell/ProfileEditViewTableCell.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileEditViewTableCell.swift
+//  Untitled
+//
+//  Created by Jaehyeok Lim on 2023/05/16.
+//
+
+import Foundation
---
+++ Untitled/Model/ProfileEditViewCellDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileEditViewDataModel.swift
+//  Untitled
+//
+//  Created by Jaehyeok Lim on 2023/05/16.
+//
+
+import Foundation
