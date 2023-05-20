7a7c9c6b3720ec49c018325cc1302981bfd0b313
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-21 00:42:06 +0900

    profileImageEditButtonTapped, tableView select 함수 추가

--- Untitled/Controller/User/UserProfileEditViewController.swift
+++ Untitled/Controller/User/UserProfileEditViewController.swift
@@ -18,7 +18,8 @@ class UserProfileEditViewController: UIViewController {
     let topDivideView = UIView()
     let bottomDivideView = UIView()
     
-    let userPofileImage = RegisterView().selectUserProfileImageView()
+//    let userPofileImage = RegisterView().selectUserProfileImageView()
+    let userPofileImage = CommonView().userProfileImageView()
     
     let profileImageEditButton = InitView().serviceButton(text: "프로필 사진변경")
     
@@ -30,6 +31,7 @@ class UserProfileEditViewController: UIViewController {
         commonUIViewLayout()
         tableViewLayout()
         navigationControllerLayout()
+        actionFunction()
     }
     
     private func viewLayout() {
@@ -38,7 +40,7 @@ class UserProfileEditViewController: UIViewController {
         userPofileImage.snp.makeConstraints { make in
             make.top.equalTo(130)
             make.centerX.equalTo(view)
-            make.size.equalTo(CGSize(width: 100, height: 100))
+            make.size.equalTo(CGSize(width: 84, height: 84))
         }
         
         profileImageEditButton.snp.makeConstraints { make in
@@ -49,7 +51,7 @@ class UserProfileEditViewController: UIViewController {
         commonUiView.snp.makeConstraints { make in
             make.top.equalTo(profileImageEditButton.snp.bottom).offset(30)
             make.leading.trailing.equalToSuperview()
-            make.height.equalTo(167.82)
+            make.height.equalTo(179.82)
         }
         
         topDivideView.snp.makeConstraints { make in
@@ -66,8 +68,8 @@ class UserProfileEditViewController: UIViewController {
         
         topDivideView.backgroundColor = UIColor.systemGray6
         bottomDivideView.backgroundColor = UIColor.systemGray6
-        userPofileImage.layer.cornerRadius = 50
-        profileImageEditButton.titleLabel?.font = UIFont(name: "NotoSansKR-Regular", size: 12)
+        userPofileImage.layer.cornerRadius = 42
+        profileImageEditButton.titleLabel?.font = UIFont(name: "NotoSansKR-Bold", size: 14)
     }
     
     private func addSubview() {
@@ -102,7 +104,7 @@ class UserProfileEditViewController: UIViewController {
         tableView.showsVerticalScrollIndicator = false
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 100, bottom: 0, right: 0)
         tableView.isScrollEnabled = false
-        tableView.rowHeight = 42
+        tableView.rowHeight = 45
     }
     
     private func navigationControllerLayout() {
@@ -121,12 +123,38 @@ class UserProfileEditViewController: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.appTextColor!]
     }
     
+    private func actionFunction() {
+        profileImageEditButton.addTarget(self, action: #selector(profileImageEditButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func profileImageEditButtonTapped() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "사진 선택", style: .default) { _ in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        })
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            alertController.addAction(UIAlertAction(title: "카메라", style: .default) { _ in
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            })
+        }
+        
+        alertController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
     @objc func cancelButtonAction(_ sender: UIBarButtonItem) {
         navigationController?.popViewController(animated: true)
     }
     
     @objc func saveButtonAction(_ sender: UIBarButtonItem) {
-        
+        dismiss(animated: true)
     }
 }
 
@@ -161,7 +189,36 @@ extension UserProfileEditViewController: UITableViewDataSource, UITableViewDeleg
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var rootViewController = UIViewController()
+        switch indexPath.row {
+        case 0:
+            rootViewController = UserProfileNameSelectViewController()
+            show(rootViewController, sender: nil)
+        case 1:
+            rootViewController = UserProfileBirthSelectViewController()
+            show(rootViewController, sender: nil)
+        case 2:
+            rootViewController = UserProfileGenderSelectViewController()
+            show(rootViewController, sender: nil)
+        default:
+            break
+        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
+
+extension UserProfileEditViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        if let pickedImage = info[.originalImage] as? UIImage {
+            userPofileImage.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
