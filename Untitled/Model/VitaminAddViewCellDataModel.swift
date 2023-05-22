c5a744e42af83eeee5d544a307c4e0f2b04cdec8
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-23 07:30:45 +0900

    vitaminAddViewCellData 추가

--- Untitled/Model/VitaminAddViewCellDataModel.swift
+++ Untitled/Model/VitaminAddViewCellDataModel.swift
@@ -5,4 +5,16 @@
 //  Created by Jaehyeok Lim on 2023/05/23.
 //
 
-import Foundation
+import UIKit
+
+struct VitaminAddViewCellDataModel {
+    let title: String
+    let isHidden: Bool
+}
+
+let vitaminAddViewCellData = [
+    ProfileEditViewCellDataModel(title: "이름", isHidden: false),
+    ProfileEditViewCellDataModel(title: "생년월일", isHidden: false),
+    ProfileEditViewCellDataModel(title: "성별", isHidden: false),
+    ProfileEditViewCellDataModel(title: "기기번호", isHidden: false)
+]
