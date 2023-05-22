b4e862845d3e4d4181c1f958d621381675de516c
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-23 07:30:14 +0900

    레이아웃 작성

--- Untitled/Controller/Vitamin/VitaminAddViewController.swift
+++ Untitled/Controller/Vitamin/VitaminAddViewController.swift
@@ -5,4 +5,121 @@
 //  Created by Jaehyeok Lim on 2023/05/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class VitaminAddViewController: UIViewController {
+    
+    let vitaminImage = CommonView().userProfileImageView()
+
+    let vitaminImageEditButton = InitView().serviceButton(text: "영양제 사진변경")
+
+    var uiViewList: [UIView] = []
+    var commonViewList: [UIView] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubview()
+        viewLayout()
+        navigationControllerLayout()
+        actionFunction()
+    }
+    
+    private func viewLayout() {
+        view.backgroundColor = UIColor.appMainBackgroundColor
+        
+        let dividerView = UIView(frame: CGRect(x: 0, y: navigationController?.navigationBar.frame.maxY ?? 0, width: view.frame.width, height: 1))
+        dividerView.backgroundColor = UIColor.systemGray6
+        navigationController?.view.addSubview(dividerView)
+        
+        vitaminImage.snp.makeConstraints { make in
+            make.top.equalTo(90)
+            make.centerX.equalTo(view)
+            make.size.equalTo(CGSize(width: 84, height: 84))
+        }
+        
+        vitaminImageEditButton.snp.makeConstraints { make in
+            make.top.equalTo(vitaminImage.snp.bottom)
+            make.width.equalTo(view)
+        }
+        
+        vitaminImage.image = UIImage()
+        vitaminImage.layer.cornerRadius = 42
+        
+        vitaminImageEditButton.titleLabel?.font = UIFont(name: "NotoSansKR-Bold", size: 14)
+    }
+    
+    private func addSubview() {
+        uiViewList = [vitaminImage, vitaminImageEditButton]
+        
+        for uiView in uiViewList {
+            view.addSubview(uiView)
+        }
+    }
+    
+    private func navigationControllerLayout() {
+        navigationItem.title = "영양제 추가"
+        
+        let cancelButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelButtonAction))
+        cancelButton.tintColor = .appTextColor
+        navigationItem.leftBarButtonItem = cancelButton
+        
+        let addButton = UIBarButtonItem(title: "추가", style: .done, target: self, action: #selector(addButtonAction))
+        addButton.tintColor = .appPointColor
+        navigationItem.rightBarButtonItem = addButton
+        
+        navigationController?.navigationBar.barTintColor = .black
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.appTextColor!]
+    }
+    
+    private func actionFunction() {
+        vitaminImageEditButton.addTarget(self, action: #selector(vitaminImageEditButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func vitaminImageEditButtonTapped() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "사진 선택", style: .default) { _ in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        })
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            alertController.addAction(UIAlertAction(title: "카메라", style: .default) { _ in
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            })
+        }
+        
+        alertController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @objc func cancelButtonAction() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func addButtonAction() {
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension VitaminAddViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        if let pickedImage = info[.originalImage] as? UIImage {
+            vitaminImage.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
