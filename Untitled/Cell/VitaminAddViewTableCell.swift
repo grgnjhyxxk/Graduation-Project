0ffcf6da893215c036bdc62b17dc5e5459e33a58
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-23 19:13:22 +0900

    코드 대거 수정

--- Untitled/Cell/VitaminAddViewTableCell.swift
+++ Untitled/Cell/VitaminAddViewTableCell.swift
@@ -13,6 +13,7 @@ import SnapKit
 // 영양제 섭취량
 
 class VitaminAddViewTableCell: UITableViewCell {
+    
     var contentViewList: [UIView] = []
     
     let titleLabel: UILabel = {
@@ -23,34 +24,46 @@ class VitaminAddViewTableCell: UITableViewCell {
         return label
     }()
     
+    let greaterthan: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.image = UIImage(systemName: "greaterthan")
+        imageView.tintColor = UIColor.placeholderText
+        
+        return imageView
+    }()
+    
     let nameTextField: UITextField = {
         let textField = UITextField()
         
         textField.backgroundColor = UIColor.clear
-        textField.textColor = UIColor.subTextColor
-        textField.textAlignment = .left
+        textField.placeholder = "종합비타민"
+        textField.textColor = UIColor.placeholderText
+        textField.textAlignment = .right
         textField.font = UIFont(name: "NotoSansKR-Regular", size: 17)
         
         return textField
     }()
     
-    let ingredientsTextField: UITextField = {
-        let textField = UITextField()
+    let ingredientsTextLabel: UILabel = {
+        let label = UILabel()
         
-        textField.backgroundColor = UIColor.clear
-        textField.textColor = UIColor.subTextColor
-        textField.textAlignment = .left
-        textField.font = UIFont(name: "NotoSansKR-Regular", size: 17)
+        label.backgroundColor = UIColor.clear
+        label.text = "비타민A, 비타민B, 비타민C ..."
+        label.textColor = UIColor.placeholderText
+        label.textAlignment = .right
+        label.font = UIFont(name: "NotoSansKR-Regular", size: 17)
         
-        return textField
+        return label
     }()
     
     let dosageTextField: UITextField = {
         let textField = UITextField()
         
         textField.backgroundColor = UIColor.clear
-        textField.textColor = UIColor.subTextColor
-        textField.textAlignment = .left
+        textField.placeholder = "1일 1정"
+        textField.textColor = UIColor.placeholderText
+        textField.textAlignment = .right
         textField.font = UIFont(name: "NotoSansKR-Regular", size: 17)
         
         return textField
@@ -62,24 +75,30 @@ class VitaminAddViewTableCell: UITableViewCell {
             make.leading.equalTo(12.5)
         }
         
+        greaterthan.snp.makeConstraints { make in
+            make.centerY.equalTo(contentView)
+            make.trailing.equalTo(-10)
+            make.size.equalTo(CGSize(width: 13, height: 23))
+        }
+        
         nameTextField.snp.makeConstraints { make in
-            make.top.equalTo(9)
-            make.leading.equalTo(100)
+            make.centerY.equalTo(contentView)
+            make.trailing.equalTo(-10)
         }
         
-        ingredientsTextField.snp.makeConstraints { make in
-            make.top.equalTo(9)
-            make.leading.equalTo(nameTextField)
+        ingredientsTextLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(contentView)
+            make.trailing.equalTo(greaterthan.snp.leading).offset(-3)
         }
         
-        ingredientsTextField.snp.makeConstraints { make in
-            make.top.equalTo(9)
-            make.leading.equalTo(nameTextField)
+        dosageTextField.snp.makeConstraints { make in
+            make.centerY.equalTo(contentView)
+            make.trailing.equalTo(-10)
         }
     }
     
     private func addSubview() {
-        contentViewList = [titleLabel, nameTextField, ingredientsTextField, dosageTextField]
+        contentViewList = [titleLabel, greaterthan, nameTextField, ingredientsTextLabel, dosageTextField]
         
         for uiView in contentViewList {
             contentView.addSubview(uiView)
@@ -88,15 +107,17 @@ class VitaminAddViewTableCell: UITableViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        greaterthan.isHidden = true
         nameTextField.isHidden = true
-        ingredientsTextField.isHidden = true
+        ingredientsTextLabel.isHidden = true
         dosageTextField.isHidden = true
     }
     
-    func hiddenFucntion(titleLabelText: String, nameTextFieldBool: Bool, ingredientsTextFieldBool: Bool, dosageTextFieldBool: Bool) {
+    func hiddenFucntion(titleLabelText: String, nameTextFieldBool: Bool, ingredientsTextLabelBool: Bool, dosageTextFieldBool: Bool, greaterthanBool: Bool) {
         titleLabel.text = titleLabelText
+        greaterthan.isHidden = greaterthanBool
         nameTextField.isHidden = nameTextFieldBool
-        ingredientsTextField.isHidden = ingredientsTextFieldBool
+        ingredientsTextLabel.isHidden = ingredientsTextLabelBool
         dosageTextField.isHidden = dosageTextFieldBool
     }
     
