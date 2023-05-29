749e582a2480f57d5f81cc35edac0c54261bbd01
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-29 22:35:56 +0900

    영양제 삭제 네트워킹 구현

--- Untitled/Network/VitaminDeletePostNetworking.swift
+++ Untitled/Network/VitaminDeletePostNetworking.swift
@@ -5,4 +5,58 @@
 //  Created by Jaehyeok Lim on 2023/05/29.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+
+func vitaminDelete(index: Int, completion: @escaping (Bool) -> Void) {
+    DispatchQueue.main.async {
+        showLoadingScreen()
+        let serverURLString = "\(serverURL)/vitamin/remove" // 서버 주소
+        
+        guard let seq = userDataList.first?.seq else {
+            print("사용자 데이터가 없습니다.")
+            return
+        }
+
+        let prod_name = userVitaminDataList[index].prod_name
+        
+        print("\(seq), \(prod_name)")
+        
+        checkServerConnection() { isConnected in
+            if isConnected {
+                let header : HTTPHeaders = [
+                    "Content-Type" : "application/json"
+                ]
+                
+                let bodyData: Parameters = [
+                    "seq": seq,
+                    "prod_name": prod_name,
+                ]
+                
+                AF.request(
+                    serverURLString,
+                    method: .post,
+                    parameters: bodyData,
+                    encoding: JSONEncoding.default,
+                    headers: header
+                )
+                .validate(statusCode: 200..<300)
+                .responseData { response in
+                    hideLoadingScreen()
+                    if response.response?.statusCode == 200 {
+                        completion(true)
+                        print("영양제 삭제 네트워킹 성공!")
+                    } else {
+                        completion(false)
+                        print("영양제 삭제 네트워킹 실패..")
+                    }
+                }
+            } else {
+                completion(false)
+                print("영양제 삭제 네트워킹 연결 실패")
+                networkErrorHandlingAlert()
+                hideLoadingScreen()
+            }
+        }
+    }
+}
