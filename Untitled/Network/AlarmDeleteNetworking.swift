bf953b65d2e12c3d2345b4b7e642be199a4c657c
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-31 18:58:14 +0900

    알람 정보 삭제 함수 작성

--- Untitled/Network/AlarmDeleteNetworking.swift
+++ Untitled/Network/AlarmDeleteNetworking.swift
@@ -5,4 +5,58 @@
 //  Created by Jaehyeok Lim on 2023/05/30.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+
+func alarmDelete(index: Int, completion: @escaping (Bool) -> Void) {
+    DispatchQueue.main.async {
+        showLoadingScreen()
+        let serverURLString = "\(serverURL)/alarm/remove" // 서버 주소
+        
+        guard let seq = userDataList.first?.seq else {
+            print("사용자 데이터가 없습니다.")
+            return
+        }
+
+        let alarm_name = userAlarmDataList[index].label
+        
+        print("\(seq), \(alarm_name)")
+        
+        checkServerConnection() { isConnected in
+            if isConnected {
+                let header : HTTPHeaders = [
+                    "Content-Type" : "application/json"
+                ]
+                
+                let bodyData: Parameters = [
+                    "seq": seq,
+                    "alarmname": alarm_name,
+                ]
+                
+                AF.request(
+                    serverURLString,
+                    method: .post,
+                    parameters: bodyData,
+                    encoding: JSONEncoding.default,
+                    headers: header
+                )
+                .validate(statusCode: 200..<300)
+                .responseData { response in
+                    hideLoadingScreen()
+                    if response.response?.statusCode == 200 {
+                        completion(true)
+                        print("알람 삭제 네트워킹 성공!")
+                    } else {
+                        completion(false)
+                        print("알람 삭제 네트워킹 실패..")
+                    }
+                }
+            } else {
+                completion(false)
+                print("알람 삭제 네트워킹 연결 실패")
+                networkErrorHandlingAlert()
+                hideLoadingScreen()
+            }
+        }
+    }
+}
