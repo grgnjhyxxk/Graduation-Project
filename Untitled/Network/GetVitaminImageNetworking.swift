a0ceefb2cffe74c811e50fd7cffdab88caf18941
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-05-31 18:56:21 +0900

    비타민 이미지 가져오는 함수 작성

--- Untitled/Network/GetVitaminImageNetworking.swift
+++ Untitled/Network/GetVitaminImageNetworking.swift
@@ -5,4 +5,31 @@
 //  Created by Jaehyeok Lim on 2023/05/30.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+
+func getVitaminImage(seq: Int, completion: @escaping (Bool) -> Void) {
+    DispatchQueue.main.async {
+        userProfileImageList.removeAll()
+        showLoadingScreen()
+        let imageUrl = "\(serverURL)/sendimage"
+        
+        AF.request(imageUrl).responseData { response in
+            switch response.result {
+            case .success(let imageData):
+                if let image = UIImage(data: imageData) {
+                    userProfileImageList.append(UserProfileImage(image: image))
+                    print("영양제 이미지 가져오기 성공")
+                    hideLoadingScreen()
+                    completion(true)
+                }
+            case .failure(let error):
+                userProfileImageList.append(UserProfileImage(image: UIImage()))
+                print("영양제 이미지 가져오기 실패: \(error)")
+                hideLoadingScreen()
+                networkErrorHandlingAlert()
+                completion(false)
+            }
+        }
+    }
+}
