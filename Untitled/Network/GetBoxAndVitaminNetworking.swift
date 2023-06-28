b1163c4e8ab9bc69b9a620f9c6962ae031272086
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-06-28 21:06:23 +0900

    박스 정보 네트워킹

--- Untitled/Network/GetBoxAndVitaminNetworking.swift
+++ Untitled/Network/GetBoxAndVitaminNetworking.swift
@@ -5,4 +5,46 @@
 //  Created by Jaehyeok Lim on 2023/06/01.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+
+struct BoxAndVitaminInformation: Decodable {
+    let boxnum: Int
+    let contain_vitamin: Int
+}
+
+func getBoxAndVitamin(completion: @escaping (Bool) -> Void) {
+    DispatchQueue.main.async {
+        showLoadingScreen()
+        let serverURLString = "\(serverURL)/box/getbox"
+        
+        let seq = userDataList[0].seq
+        
+        let parameters: Parameters = [
+            "seq": seq
+        ]
+        
+        AF.request(serverURLString, method: .get, parameters: parameters)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: [BoxAndVitaminInformation].self) { response in
+                switch response.result {
+                case .success(let boxAndVitaminInformation):
+                    print("박스 정보 가져오기 성공: \n\(boxAndVitaminInformation)")
+                    for i in 0..<boxAndVitaminInformation.count {
+                        let box = boxAndVitaminInformation[i].boxnum
+                        let vseq = boxAndVitaminInformation[i].contain_vitamin
+                        boxAndVitaminDataList.append(BoxAndVitaminDataModel(box: box, vseq: vseq))
+                    }
+                    
+                    hideLoadingScreen()
+                    completion(true)
+                case .failure(let error):
+                    print("박스 정보 가져오기 실패: \(error)")
+                    hideLoadingScreen()
+                    networkErrorHandlingAlert()
+                    completion(false)
+                }
+            }
+    }
+}
+
