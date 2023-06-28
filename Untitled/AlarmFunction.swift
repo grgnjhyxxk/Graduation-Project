badf198c1d97c5e07ff571131cbb0f7e265b70f3
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-06-28 20:58:16 +0900

    app alarm 기능

--- Untitled/AlarmFunction.swift
+++ Untitled/AlarmFunction.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-func scheduleVitaminAlarm(time: String) {
+// seq box intake vseq
+func scheduleVitaminAlarm() {
     DispatchQueue.global().async {
         // UNUserNotificationCenter 싱글턴 인스턴스를 가져옴
         let center = UNUserNotificationCenter.current()
@@ -18,37 +19,70 @@ func scheduleVitaminAlarm(time: String) {
             guard granted else { return }
             
             // 알림 콘텐츠 생성
-            let alarmName = userAlarmDataList.last?.label
-            var vitamin = String()
-            let box = userAlarmDataList.last?.numOfBox
+            let time = userAlarmDataList.first?.date
+            let alarmName = userAlarmDataList.first?.label
+            let box = userAlarmDataList.first?.box // 12
+            
+            print("\(alarmName!) \(box!)")
+            
+            // 문자열 나누기
+            var boxList: [String] = []
+
+            if let box = box {
+                let components = box.components(separatedBy: " ")
+                boxList = components
+            }
+
+            print(boxList)
+            
+            // vseq 찾기
+            var vseqList = [Int]()
+            
+            for i in boxList {
+                vseqList.append(boxAndVitaminDataList[Int(i)!-1].vseq)
+            }
+            
+            // vitaminName 찾기
+            var vitaminList = [String]()
             
             for i in 0..<userVitaminDataList.count {
-                if userAlarmDataList.last?.vitamins == userVitaminDataList[i].prod_name {
-                    vitamin = userVitaminDataList[i].prod_name
+                for j in 0..<vseqList.count {
+                    if userVitaminDataList[i].vseq == vseqList[j] {
+                        vitaminList.append(userVitaminDataList[i].prod_name)
+                    }
                 }
             }
             
+            var vitaminNames = String()
+            
+            for i in vitaminList {
+                vitaminNames += i
+                vitaminNames += " "
+            }
+            
+            print("\(String(describing: alarmName)), \(vitaminNames)")
+            
             let content = UNMutableNotificationContent()
-            content.title = "\(alarmName!) 알림!"
-            content.body = "\(vitamin)를 복용할 시간입니다."
+            content.title = "\(alarmName!)"
+            content.body = "\(vitaminNames)를 복용할 시간입니다."
             content.sound = UNNotificationSound.default
             
             // 시간 문자열을 DateComponents로 변환
             let formatter = DateFormatter()
             formatter.dateFormat = "HH:mm"
-            guard let date = formatter.date(from: time) else {
+            guard let date = formatter.date(from: time!) else {
                 print("올바른 시간 형식이 아닙니다.")
                 return
             }
             let components = Calendar.current.dateComponents([.hour, .minute], from: date)
             
-            // 알림 트리거 생성
+//             알림 트리거 생성
             let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
-            
+
             // 알림 요청 생성
             let request = UNNotificationRequest(identifier: "\(box!)", content: content, trigger: trigger)
             
-            // 알림을 스케줄에 추가
+//             알림을 스케줄에 추가
             center.add(request) { (error) in
                 if let error = error {
                     print("알림을 스케줄에 추가하는 도중 에러 발생: \(error)")
