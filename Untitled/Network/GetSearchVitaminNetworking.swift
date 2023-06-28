7e5866b00cea537b42948fb24d6ec4072506426c
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-06-28 21:08:01 +0900

    영양제 검색을 통한 추천 영양제의 영양제 정보 GET 네트워킹

--- Untitled/Network/GetSearchVitaminNetworking.swift
+++ Untitled/Network/GetSearchVitaminNetworking.swift
@@ -1,59 +1,75 @@
 //
-//  AiVitaminProdNamePostNetworking.swift
+//  GetSearchVitaminNetworking.swift
 //  Untitled
 //
 //  Created by Jaehyeok Lim on 2023/06/02.
 //
 
-import UIKit
 import Alamofire
 
 struct VitaminAiInformation: Decodable {
-    let vitaminA: Int
-    let vitaminD: Int
-    let vitaminE: Int
-    let vitaminK: Int
-    let vitaminB1: Int
-    let vitaminB2: Int
-    let vitaminB6: Int
-    let vitaminB12: Int
-    let vitaminC: Int
-    let nicotinic_acid: Int
-    let pantothenic: Int
-    let folic_acid: Int
-    let biotin: Int
-    let calcium: Int
-    let magnesium: Int
-    let iron: Int
-    let copper: Int
-    let selenium: Int
-    let iodine: Int
-    let manganese: Int
-    let molybdenum: Int
-    let chrome: Int
+    let vitaminA: Float
+    let vitaminD: Float
+    let vitaminE: Float
+    let vitaminK: Float
+    let vitaminB1: Float
+    let vitaminB2: Float
+    let vitaminB6: Float
+    let vitaminB12: Float
+    let vitaminC: Float
+    let nicotinic_acid: Float
+    let pantothenic: Float
+    let folic_acid: Float
+    let biotin: Float
+    let calcium: Float
+    let magnesium: Float
+    let iron: Float
+    let copper: Float
+    let selenium: Float
+    let iodine: Float
+    let manganese: Float
+    let molybdenum: Float
+    let chrome: Float
     let intake_per_day: Int
     let success: Int
 }
 
-func searchVitamin() {
-    let serverURL = "\(serverURL)"
-    let endpoint = "/vitamin/search"
-    
-    let parameters: Parameters = [
-        "finddata": "센트룸 맨"
-    ]
-    
-    AF.request(serverURL + endpoint, method: .post, parameters: parameters, encoding: JSONEncoding.default)
-        .validate(statusCode: 200..<300)
-        .responseDecodable(of: VitaminAiInformation.self) { response in
-            switch response.result {
-            case .success(let vitaminInfo):
-                // Handle the decoded data here
-                print("Vitamin information:")
-                print(vitaminInfo)
-            case .failure(let error):
-                // Handle the error here
-                print("Failed to fetch vitamin information: \(error)")
+func getSearchVitamin(prodname: String, completion: @escaping (Bool) -> Void) {
+    DispatchQueue.main.async {
+        showLoadingScreen()
+        let serverURLString = "\(serverURL)/vitamin/search"
+        
+        let parameters: Parameters = [
+            "prodname": prodname
+        ]
+        
+//        if prodname != "센트룸 맨" {
+//            print("DB에 없는 영양소.")
+//            completion(false)
+//        }
+        
+        AF.request(serverURLString, method: .get, parameters: parameters)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: VitaminAiInformation.self) { response in
+                switch response.result {
+                case .success(let vitaminAiInformation):
+                    print("비타민 검색 정보를 받아오는 데 성공하였습니다.")
+                    print("받은 비타민 검색 정보:")
+                    
+                    print(vitaminAiInformation)
+                    
+                    userVitaminSearchDataList.append(UserVitaminData(vseq: -1, prod_name: prodname, intake_per_day: vitaminAiInformation.intake_per_day, taken: 0, image: Data(), vitamin: [Vitamin(vitaminA: Int(vitaminAiInformation.vitaminA), vitaminD: Int(vitaminAiInformation.vitaminD), vitaminE: Int(vitaminAiInformation.vitaminE), vitaminK: Int(vitaminAiInformation.vitaminK), vitaminB1: Int(vitaminAiInformation.vitaminB1), vitaminB2: Int(vitaminAiInformation.vitaminB2), vitaminB6: Int(vitaminAiInformation.vitaminB6), vitaminB12: Int(vitaminAiInformation.vitaminB12), vitaminC: Int(vitaminAiInformation.vitaminC), nicotinic_acid: Int(vitaminAiInformation.nicotinic_acid), pantothenic: Int(vitaminAiInformation.pantothenic), folic_acid: Int(vitaminAiInformation.folic_acid), biotin: Int(vitaminAiInformation.biotin), calcium: Int(vitaminAiInformation.calcium), magnesium: Int(vitaminAiInformation.magnesium), iron: Int(vitaminAiInformation.iron), copper: Int(vitaminAiInformation.copper), selenium: Int(vitaminAiInformation.selenium), iodine: Int(vitaminAiInformation.iodine), manganese: Int(vitaminAiInformation.manganese), molybdenum: Int(vitaminAiInformation.molybdenum), chrome: Int(vitaminAiInformation.chrome))]))
+
+                    noneZeroVitaminSearch()
+                    
+                    hideLoadingScreen()
+                    completion(true)
+                case .failure(let error):
+                    print("비타민 검색 정보를 받아오는 데 실패했습니다: \(error.localizedDescription)")
+                    hideLoadingScreen()
+                    networkErrorHandlingAlert()
+                    completion(false)
+                }
             }
-        }
+    }
 }
