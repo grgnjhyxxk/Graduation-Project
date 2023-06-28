98b0778ea79474971b92fe53bb1c7a0ebabdd045
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-06-28 21:05:02 +0900

    sendRequestWithJSONPayloadSearch 추가

--- Untitled/Network/VitaminPostNetworking.swift
+++ Untitled/Network/VitaminPostNetworking.swift
@@ -66,5 +66,67 @@ func sendRequestWithJSONPayload(completion: @escaping (Bool) -> Void) {
     }
 }
 
+func sendRequestWithJSONPayloadSearch(completion: @escaping (Bool) -> Void) {
+    showLoadingScreen()
+    
+    guard let seq = userDataList.first?.seq else {
+        print("사용자 데이터가 없습니다.")
+        return
+    }
+
+    guard let basicData = userVitaminSearchDataList.last else {
+        print("영양제 기본 데이터가 없습니다.")
+        return
+    }
+
+    let serverURLString = "\(serverURL)/vitamin/add" // 서버 URL을 입력해주세요
+
+    checkServerConnection() { isConnected in
+        if isConnected {
+            let header : HTTPHeaders = [
+                "Content-Type" : "application/json"
+            ]
+            
+            var bodyData: Parameters = [
+                "seq": seq,
+                "prod_name": basicData.prod_name,
+                "intake_per_day": basicData.intake_per_day
+            ]
+            
+            for ingredient in ingredientsCellDataList {
+                bodyData[ingredient.name] = ingredient.content
+            }
+            
+            for i in 0..<vitaminSearchNames[0].count {
+                bodyData[vitaminSearchNames[0][i]] = vitaminSearchValues[0][i]
+            }
+            
+            AF.request(
+                serverURLString,
+                method: .post,
+                parameters: bodyData,
+                encoding: JSONEncoding.default,
+                headers: header
+            )
+            .validate(statusCode: 200..<300)
+            .responseData { response in
+                hideLoadingScreen()
+                if response.response?.statusCode == 200 {
+                    completion(true)
+                    print("영양제 데이터 전송 성공!")
+                } else {
+                    completion(false)
+                    print("영양제 데이터 전송 실패..")
+                }
+            }
+        } else {
+            completion(false)
+            print("영양제 데이터 전송 연결 실패")
+            networkErrorHandlingAlert()
+            hideLoadingScreen()
+        }
+    }
+}
+
 
 
