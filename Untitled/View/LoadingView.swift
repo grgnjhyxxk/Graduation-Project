74ee16b37cd5e65974d036749ad13f189afd00a5
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-11-02 22:35:11 +0900

    새로운 LoadingView 추가

--- Untitled/View/LoadingView.swift
+++ Untitled/View/LoadingView.swift
@@ -27,25 +27,28 @@ var loadingView: UIView?
 //    }
 //}
 
-func showLoadingScreen() {
-    let screenSize = UIScreen.main.bounds.size
-    loadingView = UIView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
-    loadingView?.backgroundColor = UIColor(white: 0, alpha: 0.5)
-    
-    let activityIndicator = UIActivityIndicatorView(style: .large)
-    activityIndicator.center = loadingView!.center
-    activityIndicator.startAnimating()
-    
-    loadingView?.addSubview(activityIndicator)
-    
-    if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-        if let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow }) {
-            keyWindow.addSubview(loadingView!)
-        } else if let mainWindow = windowScene.windows.first {
-            mainWindow.addSubview(loadingView!)
-        }
-    }
-}
+//func showLoadingScreen() {
+//    let screenSize = UIScreen.main.bounds.size
+//    loadingView = UIView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
+//    loadingView?.backgroundColor = UIColor(white: 0, alpha: 0.5)
+//    
+//    let activityIndicator = UIActivityIndicatorView(style: .large)
+//    activityIndicator.center = loadingView?.center ?? CGPoint.zero
+//    activityIndicator.startAnimating()
+//    
+//    loadingView?.addSubview(activityIndicator)
+//    
+//    DispatchQueue.main.async {
+//        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+//            if let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow }) {
+//                keyWindow.addSubview(loadingView!)
+//            } else if let mainWindow = windowScene.windows.first {
+//                mainWindow.addSubview(loadingView!)
+//            }
+//        }
+//    }
+//}
+
 
 
 
@@ -54,3 +57,44 @@ func hideLoadingScreen() {
     loadingView?.removeFromSuperview()
     loadingView = nil
 }
+
+
+import UIKit
+
+class LoadingView {
+    static var overlayView = UIView()
+    static var activityIndicator = UIActivityIndicatorView()
+    static var loadingLabel = UILabel()
+    
+    class func show(loadingText: String) {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            if let window = windowScene.windows.first {
+                overlayView.frame = CGRect(x: 0, y: 0, width: window.frame.width, height: window.frame.height)
+                overlayView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+                
+                activityIndicator.style = .large
+                activityIndicator.center = overlayView.center
+                
+                // 위치 조정
+                let labelFrame = CGRect(x: 0, y: activityIndicator.frame.maxY + 20, width: overlayView.frame.width, height: 30)
+                loadingLabel.frame = labelFrame
+                loadingLabel.text = loadingText
+                loadingLabel.textAlignment = .center
+                loadingLabel.textColor = UIColor.appTextColor
+                
+                overlayView.addSubview(activityIndicator)
+                overlayView.addSubview(loadingLabel)
+                window.addSubview(overlayView)
+                
+                activityIndicator.startAnimating()
+            }
+        }
+    }
+    
+    class func hide() {
+        activityIndicator.stopAnimating()
+        overlayView.removeFromSuperview()
+    }
+}
+
+
